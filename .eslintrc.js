7b54df13c42ee2eb3e2836cd58569d098b597f40
liangjiancang <liangjiancang@foxmail.com>
2020-07-16 19:58:10 +0800

    项目配置更新

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+    'module': true,
+    'GM_info': true,
+    'GM_addStyle': true,
+    'GM_getValue': true,
+    'GM_setValue': true,
+    'GM_registerMenuCommand': true,
+    'GM_xmlhttpRequest': true,
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
+  'rules': {
+    'indent': ['error', 2, {
+      'SwitchCase': 1
+    }],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-debugger': 'warn',
+  },
+}
