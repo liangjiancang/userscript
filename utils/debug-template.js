cb93274f1e895d03382bada1e27013de16b3c708
liangjiancang <liangjiancang@foxmail.com>
2020-08-03 00:45:59 +0800

    增加调试脚本模板

---
+++ utils/debug-template.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name            debug
+// @namespace       http://tampermonkey.net/
+// @version         0-debug
+// @description     Debug local script
+// @author          Laster2800
+// @require         file:///${scriptPath}
+// @[otherAttr]     [Same as target script]
+// ==/UserScript==
+
+/*
+ * 复制以上内容建立一个新的脚本，将 ${scriptPath} 替换为要调试的脚本路径，然后将目标脚本的其他脚本属性值复制进来。
+ */
