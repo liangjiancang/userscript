107911b57024bb0091b88e3f2fc4df1c00c47915
liangjiancang <liangjiancang@foxmail.com>
2020-09-10 19:55:41 +0800

    更新 API

--- BilibiliWatchlaterPlus/BilibiliWatchlaterPlus.js
+++ BilibiliWatchlaterPlus/BilibiliWatchlaterPlus.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站稍后再看功能增强
-// @version         4.5.7.20200909
+// @version         4.5.8.20200910
 // @namespace       laster2800
 // @author          Laster2800
 // @description     与稍后再看功能相关，一切你能想到和想不到的功能
@@ -15,7 +15,7 @@
 // @include         *://space.bilibili.com/*
 // @include         *://account.bilibili.com/*
 // @exclude         *://message.bilibili.com/pages/*
-// @require         https://greasyfork.org/scripts/409641-api/code/API.js?version=846211
+// @require         https://greasyfork.org/scripts/409641-api/code/API.js?version=846534
 // @grant           GM_addStyle
 // @grant           GM_xmlhttpRequest
 // @grant           GM_registerMenuCommand
