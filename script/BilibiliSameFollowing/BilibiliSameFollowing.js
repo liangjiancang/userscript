71870758c4a0f432612727ae668e67157379ef18
liangjiancang <liangjiancang@foxmail.com>
2021-06-29 19:53:14 +0800

    更新 API 等

--- script/BilibiliSameFollowing/BilibiliSameFollowing.js
+++ script/BilibiliSameFollowing/BilibiliSameFollowing.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站共同关注快速查看
-// @version         1.3.2.20210629
+// @version         1.3.3.20210629
 // @namespace       laster2800
 // @author          Laster2800
 // @description     快速查看与特定用户的共同关注（视频播放页、动态页、用户空间）
@@ -14,7 +14,7 @@
 // @exclude         *://t.bilibili.com/h5/dynamic/specification
 // @exclude         *://www.bilibili.com/page-proxy/game-nav.html
 // @exclude         /.*:\/\/.*:\/\/.*/
-// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=945119
+// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=945209
 // @grant           GM_addStyle
 // @grant           GM_notification
 // @grant           GM_xmlhttpRequest
