104359e1fe23d2a22c0e2e0a6f9154bffd0c74d9
liangjiancang <liangjiancang@foxmail.com>
2021-07-15 16:05:52 +0800

    更新 API

--- script/S1EShield/S1EShield.js
+++ script/S1EShield/S1EShield.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            S1战斗力屏蔽
 // @namespace       laster2800
-// @version         3.3.2.20210712
+// @version         3.3.3.20210715
 // @author          Laster2800
 // @description     屏蔽 S1 的战斗力系统，眼不见为净
 // @author          Laster2800
@@ -9,7 +9,7 @@
 // @homepage        https://greasyfork.org/zh-CN/scripts/394407
 // @supportURL      https://greasyfork.org/zh-CN/scripts/394407/feedback
 // @license         LGPL-3.0
-// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=949715
+// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=950686
 // @match           *.saraba1st.com/*
 // @grant           GM_addStyle
 // @run-at          document-start
