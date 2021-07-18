c209a83f51cb836528ba14c18faaa21fd4882f8a
liangjiancang <liangjiancang@foxmail.com>
2021-07-18 23:51:08 +0800

    ExplicitLog_Inject 1.0.0.20210718
    * 注入版 `V1.0`：将更多代码移至主脚本。理论上来说注入版今后几乎不需要再作修改，故重置版本号为 V1.0，不再与主脚本保持相同。
    * 注入版 `V1.0`：优化注入逻辑，避免由于注入时主脚本未加载而造成的日志捕获失效。

--- script/ExplicitLog/ExplicitLog_Inject.js
+++ script/ExplicitLog/ExplicitLog_Inject.js
@@ -1,15 +1,20 @@
 /**
  * ExplicitLog_Inject
  * @file [DEBUG] 显式日志（注入版）
- * @version 2.1.0.20210718
+ * @version 1.0.0.20210718
  * @author Laster2800
  */
 
 (function() {
-  const wrappedLog = unsafeWindow.gm429521?.fn.wrappedLog
-  if (wrappedLog) {
-    for (const n of ['log', 'warn', 'error']) {
-      console[n] = wrappedLog(console, console[n], n.toUpperCase(), GM_info.script.name)
+  for (const n of ['log', 'warn', 'error']) {
+    const log = console[n]
+    console[n] = function() {
+      if (unsafeWindow.gm429521?.fn?.wrappedLog) {
+        console[n] = unsafeWindow.gm429521.fn.wrappedLog(console, log, n.toUpperCase(), GM_info.script.name)
+        console[n].apply(console, arguments)
+      } else {
+        log.apply(console, arguments)
+      }
     }
   }
 })()
