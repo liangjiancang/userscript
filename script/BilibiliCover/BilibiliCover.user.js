fc0d6e4dbc4ccacd7efb5c37994c35de9b23b507
liangjiancang <liangjiancang@foxmail.com>
2021-07-22 14:45:27 +0800

    更新 API

--- script/BilibiliCover/BilibiliCover.user.js
+++ script/BilibiliCover/BilibiliCover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站封面获取
-// @version         4.11.10.20210722
+// @version         4.11.11.20210722
 // @namespace       laster2800
 // @author          Laster2800
 // @description     B站视频播放页（普通模式、稍后再看模式）、番剧播放页、直播间添加获取封面的按钮
@@ -16,7 +16,7 @@
 // @exclude         *://live.bilibili.com/
 // @exclude         *://live.bilibili.com/?*
 // @exclude         *://live.bilibili.com/*/*
-// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=952979
+// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=953038
 // @grant           GM_addStyle
 // @grant           GM_download
 // @grant           GM_notification
--- script/BilibiliSameFollowing/BilibiliSameFollowing.user.js
+++ script/BilibiliSameFollowing/BilibiliSameFollowing.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站共同关注快速查看
-// @version         1.4.15.20210722
+// @version         1.4.16.20210722
 // @namespace       laster2800
 // @author          Laster2800
 // @description     快速查看与特定用户的共同关注（视频播放页、动态页、用户空间、直播间）
@@ -19,7 +19,7 @@
 // @exclude         *://t.bilibili.com/h5/dynamic/specification
 // @exclude         *://www.bilibili.com/watchlater/
 // @exclude         *://www.bilibili.com/page-proxy/game-nav.html
-// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=952979
+// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=953038
 // @grant           GM_addStyle
 // @grant           GM_notification
 // @grant           GM_xmlhttpRequest
