6aea3d27265c06a5f1a92253e4ec7f9099da3bfb
liangjiancang <liangjiancang@foxmail.com>
2021-07-23 10:32:59 +0800

    B站共同关注快速查看 1.4.17.20210723
    * 简化代码：replaceAll() -> replace()

--- script/BilibiliSameFollowing/BilibiliSameFollowing.user.js
+++ script/BilibiliSameFollowing/BilibiliSameFollowing.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站共同关注快速查看
-// @version         1.4.16.20210722
+// @version         1.4.17.20210723
 // @namespace       laster2800
 // @author          Laster2800
 // @description     快速查看与特定用户的共同关注（视频播放页、动态页、用户空间、直播间）
@@ -249,7 +249,7 @@
      * @param {string} [config.className=''] 显示元素的类名；若 `target` 的子孙节点中有对应元素则直接使用，否则创建之
      */
     async generalLogic(config) {
-      let sf = config.className ? config.target.querySelector(config.className.replaceAll(/(^|\s+)(?=\w)/g, '.')) : null
+      let sf = config.className ? config.target.querySelector(config.className.replace(/(^|\s+)(?=\w)/g, '.')) : null
       if (sf) {
         sf.innerHTML = ''
       }
