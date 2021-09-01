326a7cbb128634fafe37dc1ef22f59c7094cc654
liangjiancang <liangjiancang@foxmail.com>
2021-09-01 23:29:13 +0800

    small fix

--- script/BilibiliNoSpoilProgressBar/BilibiliNoSpoilProgressBar.user.js
+++ script/BilibiliNoSpoilProgressBar/BilibiliNoSpoilProgressBar.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站防剧透进度条
-// @version         2.1.10.20210901
+// @version         2.1.11.20210901
 // @namespace       laster2800
 // @author          Laster2800
 // @description     看比赛、看番总是被进度条剧透？装上这个脚本再也不用担心这些问题了
@@ -787,7 +787,6 @@
          * 处理与设置页相关的数据和元素
          */
         const processSettingItem = () => {
-          const _self = this
           gm.menu.setting.openHandler = onOpen
           gm.el.setting.fadeInDisplay = 'flex'
           el.save.onclick = onSave
@@ -2004,7 +2003,7 @@
         }
         #${gm.id} .gm-setting td > * {
           display: flex;
-          align-items: flex-end;
+          align-items: center;
           padding: 0.2em;
           border-radius: 0.2em;
         }
