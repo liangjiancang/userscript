bf559c6c67f6d708ad5a2780b543dbd306413522
liangjiancang <liangjiancang@foxmail.com>
2021-09-05 17:10:26 +0800

    sort misc meta data

--- misc/BilibiliSeoRedirect.user.js
+++ misc/BilibiliSeoRedirect.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name            B站SEO页面重定向
-// @version         1.0.4.20210905
+// @version         1.0.5.20210905
 // @namespace       laster2800
 // @author          Laster2800
-// @icon            https://www.bilibili.com/favicon.ico
 // @description     从B站 SEO 页面重定向至常规页面
+// @icon            https://www.bilibili.com/favicon.ico
 // @homepageURL     https://greasyfork.org/zh-CN/scripts/430227
 // @supportURL      https://greasyfork.org/zh-CN/scripts/430227/feedback
 // @license         LGPL-3.0
