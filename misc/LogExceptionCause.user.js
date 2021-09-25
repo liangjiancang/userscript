565d71f8ca5ed012fe8fb8988baee28685c6ce65
liangjiancang <liangjiancang@foxmail.com>
2021-09-25 17:01:49 +0800

    [DEBUG] 异常诱因日志 1.0.0.20210925
    * 初始化提交。

---
+++ misc/LogExceptionCause.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name              [DEBUG] LogExceptionCause
+// @name:zh-CN        [DEBUG] 异常诱因日志
+// @version           1.0.0.20210925
+// @namespace         laster2800
+// @author            Laster2800
+// @description       Log exception cause, see https://github.com/tc39/proposal-error-cause
+// @description:zh-CN 记录异常诱因，详见 https://github.com/tc39/proposal-error-cause
+// @homepageURL       https://greasyfork.org/zh-CN/scripts/432924
+// @supportURL        https://greasyfork.org/zh-CN/scripts/432924/feedback
+// @license           LGPL-3.0
+// @include           *
+// @grant             none
+// @run-at            document-start
+// ==/UserScript==
+
+(function() {
+  'use strict'
+
+  window.addEventListener('error', log)
+  window.addEventListener('unhandledrejection', log)
+
+  function log(event) {
+    const cause = (event.error ?? event.reason)?.cause
+    if (cause !== undefined) {
+      console.error('↓ Error is caused by', cause)
+    }
+  }
+})()
