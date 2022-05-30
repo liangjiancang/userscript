9efe39c92bbd610dcfb7cc1f0e9834db718b4898
liangjiancang <liangjiancang@foxmail.com>
2022-05-30 17:17:38 +0800

    B站点赞批量取消 0.1.0.20220530
    - initial commit.

---
+++ script/BilibiliCancelLikes/BilibiliCancelLikes.user.js
@@ -0,0 +1,128 @@
+// ==UserScript==
+// @name            B站点赞批量取消
+// @version         0.1.0.20220530
+// @namespace       laster2800
+// @author          Laster2800
+// @description     取消对于某个UP主的所有点赞
+// @icon            https://www.bilibili.com/favicon.ico
+// @homepageURL     https://greasyfork.org/zh-CN/scripts/__USIDTODO__
+// @supportURL      https://greasyfork.org/zh-CN/scripts/__USIDTODO__/feedback
+// @license         LGPL-3.0
+// @noframes
+// @include         *://space.bilibili.com/*
+// @require         https://greasyfork.org/scripts/409641-userscriptapi/code/UserscriptAPI.js?version=974252
+// @require         https://greasyfork.org/scripts/431998-userscriptapidom/code/UserscriptAPIDom.js?version=1005139
+// @require         https://greasyfork.org/scripts/432000-userscriptapimessage/code/UserscriptAPIMessage.js?version=973744
+// @require         https://greasyfork.org/scripts/432002-userscriptapiwait/code/UserscriptAPIWait.js?version=1035042
+// @require         https://greasyfork.org/scripts/432003-userscriptapiweb/code/UserscriptAPIWeb.js?version=977807
+// @grant           GM_xmlhttpRequest
+// @grant           GM_registerMenuCommand
+// @connect         api.bilibili.com
+// ==/UserScript==
+
+(function() {
+  'use strict'
+
+  const gm = {
+    id: 'gm__USIDTODO__',
+    fn: {
+      async addButton() {
+        const container = await api.wait.$('.h .h-action')
+        const button = document.createElement('div')
+        button.textContent = '取消点赞'
+        button.className = 'h-f-btn'
+        button.addEventListener('click', this.start)
+        container.prepend(button)
+      },
+      addScriptMenu() {
+        GM_registerMenuCommand('取消点赞', this.start)
+      },
+      async start() {
+        const uid = await api.message.prompt('请输入待取消点赞UP主的 UID：', /\/(\d+)([#/?]|$)/.exec(location.pathname)?.[1])
+        if (/^\d+$/.test(uid)) {
+          let pn = 1
+          pn = await api.message.prompt('从最新投稿的第几页开始执行（每页 30 项）？', '1')
+          if (!/^\d+$/.test(pn)) {
+            pn = 1
+          } else {
+            pn = Number.parseInt(pn)
+          }
+          const result = await api.message.confirm(`是否要取消对UP主 UID ${uid} 自第 ${pn} 页起的所有点赞，该操作不可撤销！`)
+          if (result) {
+            api.message.alert(`正在取消对UP主 UID ${uid} 的点赞。执行过程详见控制台，执行完毕前请勿关闭当前标签页或将当前标签页置于后台！`)
+            gm.fn.cancelDislikes(uid, pn)
+          }
+        } else if (uid !== null) {
+          api.message.alert('UID 格式错误。')
+        }
+      },
+      async cancelDislikes(uid, start) {
+        const delay = 300
+        const csrf = this.getCSRF()
+        let cancelCnt = 0
+        const ps = 30
+        let pn = start
+        let count = -1
+        let maxPn = 1
+        api.logger.info(`START: UID = ${uid}, START = ${pn}`)
+        do {
+          api.logger.info(`PAGE: ${pn} / ${count < 0 ? '?' : maxPn}`)
+          let resp = await api.web.request({
+            method: 'GET',
+            url: `http://api.bilibili.com/x/space/arc/search?mid=${uid}&pn=${pn}&ps=${ps}`,
+          }, { check: r => r.code === 0 })
+          if (count < 0) {
+            count = resp.data.page.count
+            maxPn = Math.ceil(count / ps)
+            if (pn > maxPn) {
+              pn += 1
+              break
+            }
+          }
+          const { vlist } = resp.data.list
+          for (const item of vlist) {
+            resp = await api.web.request({
+              method: 'GET',
+              url: `http://api.bilibili.com/x/web-interface/archive/has/like?bvid=${item.bvid}`,
+            }, { check: r => r.code === 0 })
+            if (resp.data !== 0) { // 已点赞视频
+              const sp = new URLSearchParams()
+              sp.append('bvid', item.bvid)
+              sp.append('like', '2') // 取消点赞
+              sp.append('csrf', csrf)
+              const r = await api.web.request({
+                method: 'POST',
+                url: 'http://api.bilibili.com/x/web-interface/archive/like',
+                data: sp,
+              })
+              if (r.code === 0) {
+                api.logger.info(`SUCCESS: ${item.title} (${item.bvid})`)
+                cancelCnt += 1
+              } else {
+                api.logger.error(`FAIL: ${item.title} (${item.bvid})`, r ?? '未知错误')
+              }
+            }
+            await this.randomDelay(delay)
+          }
+        } while (++pn < maxPn)
+        api.logger.info(`COMPLETE: 共取消点赞 ${cancelCnt} 次，执行范围为第 ${start} ~ ${pn - 1} 页`)
+        api.message.alert(`取消点赞执行完毕，共取消点赞 ${cancelCnt} 次，详细信息请查看控制台。`)
+      },
+      async randomDelay(exp) {
+        await new Promise(resolve => setTimeout(resolve, exp * (Math.random() * 0.5 + 0.75)))
+      },
+      getCSRF() {
+        return document.cookie.replace(/(?:(?:^|.*;\s*)bili_jct\s*=\s*([^;]*).*$)|^.*$/, '$1')
+      },
+    },
+  }
+
+  /* global UserscriptAPI */
+  const api = new UserscriptAPI({
+    id: gm.id,
+    label: GM_info.script.name,
+  })
+
+  gm.fn.addButton()
+  gm.fn.addScriptMenu()
+})()
---
+++ script/BilibiliCancelLikes/README.md
@@ -0,0 +1,37 @@
+# [B站点赞批量取消](https://greasyfork.org/zh-CN/scripts/__USIDTODO__)
+
+相关脚本：**[B站稍后再看功能增强](https://greasyfork.org/zh-CN/scripts/395456)**、**[B站封面获取](https://greasyfork.org/zh-CN/scripts/395575)**、**[B站共同关注快速查看](https://greasyfork.org/zh-CN/scripts/428453)**、**[B站防剧透进度条](https://greasyfork.org/zh-CN/scripts/411092)**
+
+其他脚本：**[[DEBUG] 信息显式化](https://greasyfork.org/zh-CN/scripts/429521)**、**[S1战斗力屏蔽](https://greasyfork.org/zh-CN/scripts/394407)**，以及 **[杂项](https://greasyfork.org/zh-CN/scripts?language=all&set=470770)**
+
+忽然反感以前非常喜欢的某个UP主是一个常见现象，一想到之前给他点了这么多赞就觉得很不爽，该脚本旨在解决这一困惑。求好评，求收藏💔。点击查看 [更新日志](https://gitee.com/liangjiancang/userscript/blob/master/script/BilibiliCancelLikes/changelog.md)。
+
+## 使用说明
+
+1. 进入目标UP主的个人空间，如 <https://space.bilibili.com/208259>。
+2. 点击「关注」旁的「取消点赞」，或通过脚本菜单激活功能。
+3. 详细信息请查看控制台。
+
+## FAQ
+
+Q: 我以前明明给这个UP主点赞了许多视频，但脚本只检测出几个点赞，哪里出问题了？
+
+A: 如果你确实给某视频点了赞，但脚本检测出来，那就说明B站已经将你的点赞状态丢失，只是记录到总点赞数里。此时，你再次打开那条明明点赞过的视频，会发现你能够再次点赞。在这种情况下，你只能默默忍受因为识人不善而给这位UP主贡献点赞量的痛苦了。
+
+PS: B站丢弃过于陈旧的点赞状态似乎是在这半年间发生的。至少在 2021 年，我用相同的 API 还是能够检测出多年前的点赞状态（但网页端的点赞按钮不亮，尝试点赞会失败，也无法通过网页操作取消点赞），并且能够用 API 将点赞取消。
+
+<br>
+
+Q: 出现「取消赞失败 未点赞过」字样的错误提示怎么办？
+
+A: 其实原理跟上一条还是一样的，只是B站在丢弃你的点赞状态时处理得不够干净。此时，对应的视频处于点赞与非点赞的薛定谔状态。如果你想要挽回这种局面，只需手动打开对应的视频，点赞一次并取消即可。但这种方式只能消除薛定谔状态，无法将你以前给该视频贡献的点赞量取消。
+
+<br>
+
+Q: 如果说B站会将过于陈旧的点赞状态丢弃，那这个脚本似乎就没什么意义了？
+
+A: 其实也不是没用。如果你关注的UP主翻车了，你第一时间用这个脚本帮你取消近期的点赞还是能省不少事的……
+
+*以上均为个人推测，不保真。*
+
+**Source: [Gitee](https://gitee.com/liangjiancang/userscript/tree/master/script/BilibiliCancelLikes) / [GitHub](https://github.com/liangjiancang/userscript/tree/master/script/BilibiliCancelLikes)** - *by Laster2800*
