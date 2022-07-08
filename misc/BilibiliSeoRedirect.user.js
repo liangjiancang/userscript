c8372043ca50736addeb050ea3fe0a2c798b25c1
liangjiancang <liangjiancang@foxmail.com>
2022-07-08 19:29:24 +0800

    B站SEO页面重定向 1.0.7.20220708

--- misc/BilibiliSeoRedirect.user.js
+++ misc/BilibiliSeoRedirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站SEO页面重定向
-// @version         1.0.6.20210925
+// @version         1.0.7.20220708
 // @namespace       laster2800
 // @author          Laster2800
 // @description     从B站 SEO 页面重定向至常规页面
@@ -8,6 +8,7 @@
 // @homepageURL     https://greasyfork.org/zh-CN/scripts/430227
 // @supportURL      https://greasyfork.org/zh-CN/scripts/430227/feedback
 // @license         LGPL-3.0
+// @noframes
 // @include         *://www.bilibili.com/s/video/*
 // @grant           none
 // @run-at          document-start
