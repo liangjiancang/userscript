6cd802327d34925c24eb93d765504e1502f1b5a2
liangjiancang <liangjiancang@foxmail.com>
2022-11-16 10:58:08 +0800

    B站封面获取 5.8.10.20221116
    - 封面获取：若获取到的链接指向缩略图，将其还原为原图链接再作后续处理。

--- script/BilibiliCover/BilibiliCover.user.js
+++ script/BilibiliCover/BilibiliCover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站封面获取
-// @version         5.8.9.20221009
+// @version         5.8.10.20221116
 // @namespace       laster2800
 // @author          Laster2800
 // @description     获取B站各播放页及直播间封面，支持手动及实时预览等多种模式，支持点击下载、封面预览、快速复制，可高度自定义
@@ -563,6 +563,9 @@
        */
       setCover(target, preview, url) {
         if (url) {
+          if (/@\d+\w[._]/.test(url)) { // 若 url 是缩略图，替换为原图
+            url = url.replace(/@\d+\w[._].*/, '')
+          }
           target.href = url
           target.target = '_blank'
           target.loaded = true
