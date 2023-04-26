be3f77b67e87d961d264493babe5009bb297dab7
liangjiancang <liangjiancang@foxmail.com>
2023-04-26 21:59:02 +0800

    B站稍后再看功能增强 4.33.11.20230426
    - 批量添加：读取快照时默认使用 application/json 过滤器。

--- script/BilibiliWatchlaterPlus/BilibiliWatchlaterPlus.user.js
+++ script/BilibiliWatchlaterPlus/BilibiliWatchlaterPlus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站稍后再看功能增强
-// @version         4.33.10.20230426
+// @version         4.33.11.20230426
 // @namespace       laster2800
 // @author          Laster2800
 // @description     与稍后再看功能相关，一切你能想到和想不到的功能
@@ -1803,7 +1803,7 @@
                 <button id="gm-select-all">选中全部</button>
                 <button id="gm-deselect-all">取消全部</button>
                 <button id="gm-save-snapshot">保存快照</button>
-                <button id="gm-load-snapshot"><input type="file"><span>读取快照</span></button>
+                <button id="gm-load-snapshot"><input type="file" accept="application/json"><span>读取快照</span></button>
                 <button id="gm-save-batch-params" title="已保存参数会在加载页面后自动读取。\n右键点击以重置参数，刷新页面后生效。">保存参数</button>
                 <button id="gm-load-batch-params">读取参数</button>
               </div></div>
@@ -1969,7 +1969,7 @@
               items: el.items.innerHTML,
             }
             const filename = `${gm.config.batchAddManagerSnapshotPrefix}.${webpage.method.getTimeString(null, '', '', '-')}.json`
-            const file = new Blob([JSON.stringify(snapshot)], { type: 'text/plain' })
+            const file = new Blob([JSON.stringify(snapshot)], { type: 'application/json' })
             const a = document.createElement('a')
             a.href = URL.createObjectURL(file)
             a.download = filename
